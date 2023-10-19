2981a39f15f0ea34dfeef819863c667f4825fc5b
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2023-10-19 07:44:29 -0400

    Added functionality to change grid size

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-const selectDensity = document.querySelectorAll(".density")
 const selectMode = document.querySelectorAll(".mode")
 
 /* Adds Listener to Buttons */ 
 function changeGrid () {
-    selectDensity.forEach((button) =>  {
-        button.addEventListener('click', gridSize());
+    var selectDensitySmall = document.querySelector(".small")
+    var selectDensityMedium = document.querySelector(".medium")
+    var selectDensityLarge = document.querySelector(".large")
+    selectDensitySmall.addEventListener("click", function () {
+        generateGrid(16,22);
+    });
+    selectDensityMedium.addEventListener("click", function () {
+        generateGrid(32,44);
+    });
+    selectDensityLarge.addEventListener("click", function () {
+        generateGrid(64,88);
     });
 }
 
+changeGrid()
+
 /* Changes Grid Size of Buttons */ 
-function gridSize() {
-    
-}
-
-
-function generateGrid(density) {
-    density = 64 * 64;
+function generateGrid(density, rows) {
+    console.log('hi');
+    // Set Size of Grid
+    var size = density * rows;
 
     // Identify Container
     const container = document.querySelector('.generated-grid');
@@ -23,15 +30,18 @@ function generateGrid(density) {
     // Clear the Container
     container.innerHTML = '';
 
+    // Set Columns for Grid
+    container.style.gridTemplateColumns = `repeat(${density},auto)`;
+
     // Loop to Create Containers
-    for (let x = 0; x < density; x++) {
+    for (let x = 0; x < size; x++) {
         var div = document.createElement('div');
         div.classList.add('row');
         container.appendChild(div);
     }   
 }
 
-generateGrid(8)
+generateGrid(32, 44)
 
 
 
