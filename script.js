7c5c327e286d6826addca125730985c410f163b1
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2023-10-19 16:22:26 -0400

    Finished the Software

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var color = "#C0C0C0"
 const selectMode = document.querySelectorAll(".mode")
 
 /* Adds Listener to Buttons */ 
@@ -38,35 +39,54 @@ function generateGrid(density, rows) {
         var div = document.createElement('div');
         div.classList.add('row');
         container.appendChild(div);
-    }   
+    }
+    changeColor(color);   
 }
 
 generateGrid(32, 44)
 
+/* Add a Permanent Change to Div */
+function changeColor (color) {
+    var row = document.querySelectorAll(".row")
 
-
-
-/*
-function generateGrid(density) {
-    // Identify Container
-    const container = document.querySelector('.generated-grid');
-
-    // Clear the Container
-    container.innerHTML = '';
-
-    // Loop to Create Containers
-    for (let x = 0; x < density; x++) {
-        var row = document.createElement('div');
-        row.classList.add('row');
-
-        // Loop to Create Columned Rows
-        for (let i = 0; i < density; i++) {
-        var cell = document.createElement('div')
-        cell.classList.add('cell')
-        row.appendChild(cell)
-        }
-        container.appendChild(row);
-    }   
+    row.forEach(function(hoverEvent) {
+        hoverEvent.addEventListener("mouseover", function () {
+            hoverEvent.style.backgroundColor = color;
+        });
+    });
 }
 
-generateGrid(8) */
+changeColor(color);
+
+/* Adds Listener to Buttons */ 
+function changeColorBackground () {
+    var selectClassic = document.querySelector(".classic")
+    var selectModern = document.querySelector(".modern")
+    var selectRetro = document.querySelector(".retro")
+
+    selectClassic.addEventListener("click", function () {
+        color = "#C0C0C0";
+        changeColor(color);
+    });
+    selectModern.addEventListener("click", function () {
+        color = "#D19A94";
+        changeColor(color);
+    });
+    selectRetro.addEventListener("click", function () {
+        color = "#008080";
+        changeColor(color);
+    });
+}
+
+changeColorBackground ();
+
+
+function resetBackground() {
+    var selectErase = document.querySelector(".erase-button")
+    selectErase.addEventListener("click", function () {
+        var color = "#FFFFFF"
+        changeColor(color);
+    });
+}
+
+resetBackground();
